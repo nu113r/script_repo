ae27a90bb3dbe8d0b5c571d6103fd511ce803249
nu113r <belwal.mohit@gmail.com>
2015-07-31 11:21:04 +0530

    .

--- setup_druplal.sh
+++ setup_druplal.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
-
+set -e
 function install_puppet_agent(){
-puppet --version
-if [ $? -eq 0 ]; then
-    mkdir -p /etc/puppet/environments/redcrackle/modules;mkdir -p /etc/puppet/environments/redcrackle/manifest
-    echo "puppet is already installed"
-else
-    wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb; dpkg -i puppetlabs-release-trusty.deb; sudo apt-get update;sudo apt-get -y install puppet; mkdir -p /etc/puppet/environments/redcrackle/modules;mkdir -p /etc/puppet/environments/redcrackle/manifest
-fi
+	puppet --version
+	if [ $? -eq 0 ]; then
+		echo "puppet is already installed"
+	else
+		wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+		dpkg -i puppetlabs-release-trusty.deb
+		sudo apt-get update
+		sudo apt-get -y install puppet
+                sed -i 's/no/yes/g' /etc/default/puppet
+                service puppet restart 
+	fi
+
+	mkdir -p /etc/puppet/environments/redcrackle
 }
 
 
 function download_module(){
-  repository_url="https://github.com/nu113r/alok.git"
-  git clone $repository_url
-  url=$repository_url
-  basename=$(basename $url)
-  name=${basename%.*}
-  echo "copy all modules or manifest"
-  cd ${name}/modules/ ; cp -a * /etc/puppet/environments/redcrackle/modules/. ; cd ../manifest/;  cp -a * /etc/puppet/environments/redcrackle/manifest/.   
+	repository_url="https://github.com/OpsTree/PuppetDrupalSetup.git"
+	git clone $repository_url /etc/puppet/environments/redcrackle
 }
 
  
 function apply_catalog(){
-   puppet apply --modulepath=/etc/puppet/environments/redcrackle/modules /etc/puppet/environments/redcrackle/manifest/drupal_stack.pp --debug
-   puppet apply --modulepath=/etc/puppet/environments/redcrackle/modules /etc/puppet/environments/redcrackle/manifest/deploy_drupal_app.pp --debug
+	puppet apply --modulepath=/etc/puppet/environments/redcrackle/modules /etc/puppet/environments/redcrackle/manifest/drupal_stack.pp --debug
+	puppet apply --modulepath=/etc/puppet/environments/redcrackle/modules /etc/puppet/environments/redcrackle/manifest/deploy_drupal_app.pp --debug
+}
+
+function notify_script_execution() {
+	wget google.com
 }
 
 apt-get update
